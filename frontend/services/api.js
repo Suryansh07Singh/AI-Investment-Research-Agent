01699da3ac3983c2e5bc820de34af5fbcd54ac7a
Suryansh Vikram Singh <suryanshs248@gmail.com>
2026-07-01 17:36:59 +0530

    Updated API URL

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "http://localhost:5000/api/analyze";
+  "https://ai-investment-research-agent-c2f9.onrender.com/api/analyze";
 
 export const analyzeCompany = async (companyName) => {
     try {
